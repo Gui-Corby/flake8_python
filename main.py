3153e69896724b54aea4b57cf1d96bd2faf5ca7c
Gui-Corby <guilhermecorbym@gmail.com>
2025-03-10 11:30:51 -0300

    testing flake8

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 from fila_normal import fila_normal
 from fila_prioritaria import FilaPrioritaria
 
-# fila_teste = fila_normal()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# print(fila_teste.chamar_cliente(5))
-# print(fila_teste.chamar_cliente(3))
+fila_teste = fila_normal()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+print(fila_teste.chamar_cliente(5))
+print(fila_teste.chamar_cliente(3))
 
 fila_teste_2 = FilaPrioritaria()
 fila_teste_2.atualiza_fila()
@@ -16,4 +16,4 @@ fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 print(fila_teste_2.chama_cliente(7))
 print(fila_teste_2.chama_cliente(4))
-print(fila_teste_2.estatistica('10/01/2025', 197, 'detail'))
+print(fila_teste_2.estatistica('10/01/2025', 215, 'detail'))
